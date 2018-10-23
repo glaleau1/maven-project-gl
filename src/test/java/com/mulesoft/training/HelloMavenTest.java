43e47b28ea2f24a5354fba0191c8960410199884
Geoffrey Laleau <glaleau@deloitte.com>
2018-10-23 09:54:07 -0400

    Added support for Split Global Elements into different xml files

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -28,8 +28,9 @@ public class HelloMavenTest extends FunctionalTestCase {
 	}
 
 	@Override
-	protected String getConfigFile() {
-		return "maven-project.xml";
+	protected String[] getConfigFiles() {
+		String[] files = { "common-resources.xml", "maven-project.xml" };
+		return files;
 	}
 
 }
